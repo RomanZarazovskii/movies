74b5b8ec3529f0b7d35b80fc340442ef0bb4a2d6
RomanZarazovskii <124078828+RomanZarazovskii@users.noreply.github.com>
2024-12-17 18:18:27 +0000

    Deploying to gh-pages from @ RomanZarazovskii/movies@e1eea9ef0fc2d8397e5bbf8a8802a35dfcfa641d 🚀

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();const l=document.getElementById("all-button");l.addEventListener("click",i);const u="http://localhost:3000";function i(){return fetch(`${u}/movies`).then(n=>n.json())}i();
+(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&s(r)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const c=document.getElementById("all-button"),l=document.getElementById("movies__container");c.addEventListener("click",u);const d="http://localhost:3000";function a(){return fetch(`${d}/movies`).then(o=>o.json())}function u(){a().then(f)}function f(o){const n=o.map(i=>`
+    <li class="movies__item">
+                <div>
+                    <img class="movies__image" src="${o.image}" alt="#" onerror="this.src='https://via.placeholder.com/150';">
+                    <h2 class="movies__tittle"><b>Tittle: </b>${o.title}</h2>
+                    <p class="movies__genre"><b>Genre: </b>${o.genre}</p>
+                    <p class="movies__year"><b>Year: </b>${o.year}</p>
+                </div>
+                <div>
+                    <button class="movies__item-btn" type="button" id="edit__btn">edit</button>
+                    <button class="movies__item-btn" type="button" id="delete__btn">delete</button>
+                    </div>
+                    </li>
+    `).join("");l.insertAdjacentHTML("beforeend",n)}
 //# sourceMappingURL=index.js.map
